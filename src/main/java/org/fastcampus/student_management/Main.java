497a210dfa3fa168ac83bd928f5f7ea24d6a5ec6
JeonDoGyun <wjsehrbs2632@naver.com>
2025-05-03 17:40:28 +0900

    VO, FirstClassCollection 실습 진행

--- src/main/java/org/fastcampus/student_management/Main.java
+++ src/main/java/org/fastcampus/student_management/Main.java
@@ -1,14 +1,16 @@
 package org.fastcampus.student_management;
 
 import org.fastcampus.student_management.application.course.CourseService;
+import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
+import org.fastcampus.student_management.application.student.dto.StudentInfoDto;
 import org.fastcampus.student_management.repo.CourseRepository;
 import org.fastcampus.student_management.repo.StudentRepository;
+import org.fastcampus.student_management.ui.UserInputType;
 import org.fastcampus.student_management.ui.course.CourseController;
 import org.fastcampus.student_management.ui.course.CoursePresenter;
 import org.fastcampus.student_management.ui.student.StudentController;
 import org.fastcampus.student_management.ui.student.StudentPresenter;
-import org.fastcampus.student_management.ui.UserInputType;
 
 public class Main {
 
@@ -24,6 +26,17 @@ public class Main {
 
     CourseController courseController = new CourseController(coursePresenter, courseService, studentPresenter);
     StudentController studentController = new StudentController(studentPresenter, studentService);
+
+    // Student 테스팅용 입력
+    StudentInfoDto studentInfoDto = new StudentInfoDto("홍길동", 10, "강남구");
+    StudentInfoDto studentInfoDto1 = new StudentInfoDto("김철수", 19, "노원구");
+    studentService.saveStudent(studentInfoDto);
+    studentService.saveStudent(studentInfoDto1);
+
+    CourseInfoDto courseInfoDto = new CourseInfoDto("바이올린", 1000, "MONDAY", "홍길동", 1717299008L);
+    CourseInfoDto courseInfoDto1 = new CourseInfoDto("첼로", 12000, "MONDAY", "김철수", 1717299008L);
+    courseService.registerCourse(courseInfoDto);
+    courseService.registerCourse(courseInfoDto1);
 
     studentPresenter.showMenu();
     UserInputType userInputType = studentController.getUserInput();
--- src/main/java/org/fastcampus/student_management/application/course/CourseService.java
+++ src/main/java/org/fastcampus/student_management/application/course/CourseService.java
@@ -1,10 +1,11 @@
 package org.fastcampus.student_management.application.course;
 
-import java.util.ArrayList;
 import java.util.List;
+
 import org.fastcampus.student_management.application.course.dto.CourseInfoDto;
 import org.fastcampus.student_management.application.student.StudentService;
 import org.fastcampus.student_management.domain.Course;
+import org.fastcampus.student_management.domain.CourseList;
 import org.fastcampus.student_management.domain.DayOfWeek;
 import org.fastcampus.student_management.domain.Student;
 import org.fastcampus.student_management.repo.CourseRepository;
@@ -25,11 +26,15 @@ public class CourseService {
   }
 
   public List<CourseInfoDto> getCourseDayOfWeek(DayOfWeek dayOfWeek) {
-    // TODO: 과제 구현 부분
-    return new ArrayList<>();
+    List<Course> courses = courseRepository.getCourseDayOfWeek(dayOfWeek);
+    return courses.stream().map(CourseInfoDto::new).toList();
   }
 
+  
   public void changeFee(String studentName, int fee) {
-    // TODO: 과제 구현 부분
+    List<Course> courses = courseRepository.getCourseListByStudent(studentName);
+    // 일급 컬렉션 생성해서 적용 : 장점 >> 로직 보기가 쉬움, 재사용 가능, 테스트가 쉬워짐
+    CourseList courseList = new CourseList(courses);
+    courseList.changeAllCoursesFee(fee);
   }
 }
